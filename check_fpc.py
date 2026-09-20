d29cdece3c0f14797098f43c9617a86272dfdd0c
PRANEETH-VEDAGIRI <vedagiri.praneeth@valuehealthai.com>
2026-09-20 14:58:54 +0530

    Clean repo: remove test scripts, update README with proper docs

--- check_fpc.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-conn = psycopg2.connect(host="127.0.0.1", port=5438, dbname="medivo_qa", user="postgres", password="q4$TK8((rq0e!XD9n9T.kj~_:mc$")
-cur = conn.cursor()
-cur.execute("""SELECT column_name, data_type FROM information_schema.columns
-               WHERE table_schema='crm' AND table_name='formulary_product_coverage'
-               ORDER BY ordinal_position""")
-for r in cur.fetchall():
-    print(f"  {r[0]} ({r[1]})")
-conn.close()
